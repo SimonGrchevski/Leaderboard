f9c1c729a54f1a0b98ffc13f3e06a6aef2e90e94
SimonGrchevski <simongrcevski@gmail.com>
2021-07-28 20:36:38 +0200

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    contentBase: './dist',
+    contentBase: './docs',
   },
 
   module: {
@@ -23,7 +23,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
 };
